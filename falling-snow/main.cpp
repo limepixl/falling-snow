11c8fa235b04de02a2f67a2ae95a5c56e06e70e2
Stefan Ivanovski <stefs.ivanovski@gmail.com>
2019-01-06 23:07:19 +0100

    Brace style

--- falling-snow/main.cpp
+++ falling-snow/main.cpp
@@ -9,7 +9,8 @@ const unsigned int HEIGHT = 600;
 
 void createFlake(std::vector<sf::CircleShape>& arr);
 
-int main() {
+int main()
+{
 	unsigned int elapsed = 0;		// Elapsed time since last snowflake spawn
 	srand((unsigned int)time(0));	// Random seeding
 
@@ -33,29 +34,34 @@ int main() {
 	std::vector<sf::CircleShape> snowflakes;
 	snowflakes.reserve(numOfSnowflakes);
 
-	while(window.isOpen()) {
+	while(window.isOpen())
+	{
 		elapsed++;
 
 		sf::Event event;
-		while(window.pollEvent(event)) {
+		while(window.pollEvent(event))
+		{
 			if(event.type == sf::Event::Closed)
 				window.close();
 		}
 
 		// If the delay has passed and the max num of snowflakes isn't reached, create another
-		if(elapsed >= delay && snowflakes.size() < numOfSnowflakes) {
+		if(elapsed >= delay && snowflakes.size() < numOfSnowflakes)
+		{
 			createFlake(snowflakes);
 			elapsed = 0;
 		}
 
 		window.clear();
 
-		for(unsigned int i = 0; i < snowflakes.size(); i++) {
+		for(unsigned int i = 0; i < snowflakes.size(); i++)
+		{
 			snowflakes[i].move(0.0f, snowflakes[i].getRadius() * 0.4f);
 			window.draw(snowflakes[i]);
 
 			// Don't draw snowflakes that are outside of the window
-			if(snowflakes[i].getPosition().y >= HEIGHT) {
+			if(snowflakes[i].getPosition().y >= HEIGHT)
+			{
 				snowflakes.erase(snowflakes.begin() + i);
 			}
 		}
@@ -66,7 +72,8 @@ int main() {
 }
 
 // Create a snowflake and store it in an array
-void createFlake(std::vector<sf::CircleShape>& arr) {
+void createFlake(std::vector<sf::CircleShape>& arr)
+{
 	sf::CircleShape flake(float(rand()) / (float)(RAND_MAX / 3.0f) + 0.5f);
 	flake.setPosition(float(rand() % WIDTH), 0.0f);
 	flake.setFillColor(sf::Color::White);
